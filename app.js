b55b0d6f078aed6236a83ac1ed41a59874242166
Misash <misash@outlook.com>
2022-07-21 15:21:57 -0500

    create finish

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const ejs = require("ejs")
-const date = require("./date.js")
+// const date = require("./date.js")
+const mongoose = require('mongoose');
 
 const app = express()
 var port = 3000
@@ -13,26 +14,74 @@ app.use(express.static("public"))
 app.set("view engine","ejs")
 
 
+mongoose.connect('mongodb://localhost:27017/todoListDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-var items = ["buy food" , "cook food" , "eat food"]
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({  
+    name: "Welcome to your todolist!",
+})
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+})
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+})
+
+const defaultItems = [item1 , item2 , item3]
+
+
 
 
 app.get("/", (req, res) => {
 
-   const day = date.day()
+    Item.find({},(err,items)=>{
 
-    res.render('list', { day: day , items: items});
+        if(items.length === 0){
+            Item.insertMany(defaultItems,(err)=>{
+                if(err){
+                    console.log(err)
+                }else{
+                    console.log("Successfully saved default items to DB.")
+                }
+            })
+            res.redirect("/")
+        }else{
+            res.render('list', { day: "Today" , items: items});
+        }
+    })
 
 })
 
 
 app.post("/",(req,res)=>{
-    const item = req.body.newItem
-    items.push(item)
+
+    const item = new Item({
+        name: req.body.newItem
+    })
+
+    Item.create(item ,(err,res)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log("1 document inserted.")
+        }
+    })
+    
+    // console.log(item)
+
     res.redirect("/")
 })
 
-
+app.post("/delete",(req,res)=>{
+    console.log(req.body)
+})
 
 
 app.listen(port, () => {
